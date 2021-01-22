6b24461c7828594efd081792abb0eb1f81e1270e
zhaxie <787631162@qq.com>
2021-01-22 14:47:28 +0800

    nothing

--- src/views/CAD/imgWithCovers.vue
+++ src/views/CAD/imgWithCovers.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date         : 2020-10-20 15:15:49
  * @LastEditors  : cxx
- * @LastEditTime : 2021-01-22 14:32:37
+ * @LastEditTime : 2021-01-22 14:47:19
  * @FilePath     : \small_cad\src\views\CAD\imgWithCovers.vue
 -->
 <template>
@@ -22,6 +22,7 @@
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
